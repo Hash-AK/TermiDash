9f9bc5f42e670f99c413ac998696525cbdf9cd05
Hash-AK <175879022+Hash-AK@users.noreply.github.com>
2025-11-19 14:36:24 -0500

    adding fastfetch-style info slowly

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 	"github.com/shirou/gopsutil/v4/cpu"
+	"github.com/shirou/gopsutil/v4/host"
 )
 
 func main() {
@@ -13,6 +15,7 @@ func main() {
 	cpuCountPhys, _ := cpu.Counts(false)
 	cpuCountLogical, _ := cpu.Counts(true)
 	cpuCountText := fmt.Sprintf("CPU count physical/logical: %v/%v", cpuCountPhys, cpuCountLogical)
+
 	cpuPanel := tview.NewTextView()
 	cpuPanel.SetText(cpuCountText)
 	cpuPanel.SetBorder(true)
@@ -21,12 +24,22 @@ func main() {
 	cpuPanel.SetDynamicColors(true)
 
 	//FastFetch-style section
+	KernelVersion, _ := host.KernelVersion()
+	OSPlatform, _, _, _ := host.PlatformInformation()
+	firstChar := strings.ToUpper(string(OSPlatform[0]))
+	OSPlatform = firstChar + OSPlatform[1:]
+	OSArch, _ := host.KernelArch()
+	hostInfo, _ := host.Info()
+	hostname := hostInfo.Hostname
+	OSInfoText := fmt.Sprintf("OS : %s %s\nKernel Version : %s\nHostname : %s", OSPlatform, OSArch, KernelVersion, hostname)
+
 	infoPanel := tview.NewTextView()
 	infoPanel.SetBorder(true)
 	infoPanel.SetTitle("System Information")
 	infoPanel.SetDynamicColors(true)
-
+	infoPanel.SetText(OSInfoText)
 	// Memory Section
+
 	memPanel := tview.NewTextView()
 	memPanel.SetBorder(true)
 	memPanel.SetTitle("Memory")
